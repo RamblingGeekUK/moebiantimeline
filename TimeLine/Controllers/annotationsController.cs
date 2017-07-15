b7f4efdb5e5097b8ce93e3840964e8b9c4825aee
Wayne Taylor <Wayne Taylor>
2017-07-15 22:35:06 +0100

    Add project files.

---
+++ TimeLine/Controllers/annotationsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TimeLine.Models;
+
+namespace TimeLine.Controllers
+{
+    [Authorize]
+    public class annotationsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: annotations
+        public ActionResult Index()
+        {
+            return View(db.annotations.OrderByDescending(d=> d.entrydatetime).ToList());
+        }
+
+        // GET: annotations/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            annotation annotation = db.annotations.Find(id);
+            if (annotation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(annotation);
+        }
+
+        // GET: annotations/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: annotations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,entrydatetime,datetime,subject,description,context,username")] annotation annotation)
+        {
+            if (ModelState.IsValid)
+            {
+                annotation.id = Guid.NewGuid();
+                annotation.username = User.Identity.Name;
+                db.annotations.Add(annotation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(annotation);
+        }
+
+        // GET: annotations/Create
+        public ActionResult PartialCreate()
+        {
+            return PartialView();
+        }
+
+        // POST: annotations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PartialCreate([Bind(Include = "id,entrydatetime,datetime,subject,description,context,username")] annotation annotation)
+        {
+            if (ModelState.IsValid)
+            {
+                annotation.id = Guid.NewGuid();
+                annotation.username = User.Identity.Name;
+                db.annotations.Add(annotation);
+                db.SaveChanges();
+                return PartialView();
+            }
+
+            return PartialView();
+        }
+
+
+
+        // GET: annotations/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            annotation annotation = db.annotations.Find(id);
+            if (annotation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(annotation);
+        }
+
+        // POST: annotations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,entrydatetime,datetime,subject,description,context,username")] annotation annotation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(annotation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(annotation);
+        }
+
+        // GET: annotations/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            annotation annotation = db.annotations.Find(id);
+            if (annotation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(annotation);
+        }
+
+        // POST: annotations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            annotation annotation = db.annotations.Find(id);
+            db.annotations.Remove(annotation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TimeLine/Models/Annotation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+
+namespace TimeLine.Models
+{
+    public class annotation
+    {
+
+        private DateTime? dateCreated = null;
+        public Guid id { get; set; }
+        public DateTime entrydatetime {
+            get
+            {
+                return this.dateCreated.HasValue
+                    ? this.dateCreated.Value
+                    : DateTime.Now;
+            }
+            set { this.dateCreated = value; }
+        }
+        
+        public DateTime datetime {
+            get
+            {
+                return this.dateCreated.HasValue
+                    ? this.dateCreated.Value
+                    : DateTime.Now;
+            }
+            set { this.dateCreated = value; }
+
+        }
+        public String subject { get; set; }
+        public String description { get; set; }
+        public String context { get; set; }
+
+        public string username { get; set; }
+
+    }
+}
